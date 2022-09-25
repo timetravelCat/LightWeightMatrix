e75293db31ca6e02b90662bef9f028dd7a11a067
timetraveler <timetraveler930@gmail.com>
2022-09-25 22:40:09 +0900

    implicit cast, type_helper.cpp

--- include/LightWeightMatrix/type_helper.hpp
+++ include/LightWeightMatrix/type_helper.hpp
@@ -16,15 +16,23 @@ namespace lwm
 {
   namespace internal
   {
-    template<typename T, typename T1, typename... TRest>
+    template<typename T, typename... TRest>
     struct are_floating_point
     {
-      static const bool value = are_floating_point<T, T1>::value && are_floating_point<T, TRest...>::value;
+      static const bool value = are_floating_point<T>::value && are_floating_point<TRest...>::value;
     };
-    template<typename T, typename T1>
-    struct are_floating_point<T, T1>
+    template<typename T>
+    struct are_floating_point<T> : public is_floating_point<T>
     {
-      static const bool value = is_floating_point<T>::value && is_floating_point<T1>::value;
+    };
+    template<typename T, typename... TRest>
+    struct are_fixed_point
+    {
+      static const bool value = are_fixed_point<T>::value && are_fixed_point<TRest...>::value;
+    };
+    template<typename T>
+    struct are_fixed_point<T> : public is_integral<T>
+    {
     };
     template<typename T, typename T1, typename... TRest>
     struct include_floating_point
@@ -36,11 +44,20 @@ namespace lwm
     {
       static const bool value = is_floating_point<T>::value || is_floating_point<T1>::value;
     };
-
+    template<typename T, typename T1, typename... TRest>
+    struct include_fixed_point
+    {
+      static const bool value = include_fixed_point<T, T1>::value || include_fixed_point<T, TRest...>::value;
+    };
+    template<typename T, typename T1>
+    struct include_fixed_point<T, T1>
+    {
+      static const bool value = is_integral<T>::value || is_integral<T1>::value;
+    };
     template<typename T, typename T1, typename... TRest>
     struct implicit_cast_floating_point
     {
-      // Todo 
+      typedef typename implicit_cast_floating_point<T, typename implicit_cast_floating_point<T1, TRest...>::type>::type type;
     };
     template<typename T, typename T1>
     struct implicit_cast_floating_point<T, T1>
@@ -54,9 +71,33 @@ namespace lwm
           float>
           type;
     };
-
+    template<typename T, typename T1, typename... TRest>
+    using implicit_cast_floating_point_t = typename implicit_cast_floating_point<T, T1, TRest...>::type;
+    template<typename T, typename... TRest>
+    struct include_signed
+    {
+      static const bool value = include_signed<T>::value || include_signed<TRest...>::value;
+    };
+    template<typename T>
+    struct include_signed<T> : public is_signed<T>
+    {
+    };
+    template<typename T, typename T1, typename... TRest>
+    struct implicit_cast_fixed_point
+    {
+      typedef typename implicit_cast_fixed_point<T, typename implicit_cast_fixed_point<T1, TRest...>::type>::type type;
+    };
     template<typename T, typename T1>
-    using implicit_cast_floating_point_t = typename implicit_cast_floating_point<T, T1>::type;
-    
+    struct implicit_cast_fixed_point<T, T1>
+    {
+      static_assert(!include_floating_point<T, T1>::value, "implicit_cast_fixed_point includes floating point");
+      typedef conditional_t<
+          (sizeof(T) >= sizeof(T1)),
+          conditional_t<include_signed<T, T1>::value, make_signed_t<T>, T>,
+          conditional_t<include_signed<T, T1>::value, make_signed_t<T1>, T1>>
+          type;
+    };
+    template<typename T, typename T1, typename... TRest>
+    using implicit_cast_fixed_point_t = typename implicit_cast_fixed_point<T, T1, TRest...>::type;
   };  // namespace internal
 };    // namespace lwm
--- test/src/test_type_helper.cpp
+++ test/src/test_type_helper.cpp
@@ -5,7 +5,7 @@
 using namespace lwm::internal;
 
 TEMPLATE_PRODUCT_TEST_CASE(
-    "are_floating_type testing",
+    "are_floating_point testing",
     "[template]",
     lwm::internal::are_floating_point,
     ((double, float), (float, float), (double, float, float), (double, float, float, double)))
@@ -14,7 +14,7 @@ TEMPLATE_PRODUCT_TEST_CASE(
 }
 
 TEMPLATE_PRODUCT_TEST_CASE(
-    "are_floating_type testing",
+    "are_floating_point testing",
     "[template]",
     lwm::internal::are_floating_point,
     ((double, int), (int, float), (int, int, int), (double, long, int, double)))
@@ -23,7 +23,25 @@ TEMPLATE_PRODUCT_TEST_CASE(
 }
 
 TEMPLATE_PRODUCT_TEST_CASE(
-    "include_floating_type testing",
+    "are_fixed_point testing",
+    "[template]",
+    lwm::internal::are_fixed_point,
+    ((int, long), (long, long), (int, int, int), (long long, unsigned, unsigned int, long)))
+{
+  REQUIRE(TestType::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "are_fixed_point testing",
+    "[template]",
+    lwm::internal::are_fixed_point,
+    ((int, float), (float, double), (float, float, float), (long double, unsigned, unsigned int, long)))
+{
+  REQUIRE(TestType::value == false);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "include_floating_point testing",
     "[template]",
     lwm::internal::include_floating_point,
     ((double, int), (int, float), (float, double, float), (int, long, int, double)))
@@ -32,10 +50,28 @@ TEMPLATE_PRODUCT_TEST_CASE(
 }
 
 TEMPLATE_PRODUCT_TEST_CASE(
-    "include_floating_type testing",
+    "include_floating_point testing",
     "[template]",
     lwm::internal::include_floating_point,
     ((int, int), (int, long), (int, int, int), (long long, long, int, char)))
+{
+  REQUIRE(TestType::value == false);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "include_fixed_point testing",
+    "[template]",
+    lwm::internal::include_fixed_point,
+    ((double, int), (int, int), (float, long, float), (int, long, int, double)))
+{
+  REQUIRE(TestType::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "include_fixed_point testing",
+    "[template]",
+    lwm::internal::include_fixed_point,
+    ((float, float), (double, long double), (double, double, double), (long double, float, float, double)))
 {
   REQUIRE(TestType::value == false);
 }
@@ -65,4 +101,61 @@ TEMPLATE_PRODUCT_TEST_CASE(
     ((int, long double), (float, long double), (long double, long double), (long double, long)))
 {
   REQUIRE(is_same<typename TestType::type, long double>::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "implicit_cast_floating_point testing",
+    "[template]",
+    lwm::internal::implicit_cast_floating_point,
+    ((int, int, int), (float, int, float), (long, float, float), (float, float, float)))
+{
+  REQUIRE(is_same<typename TestType::type, float>::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "implicit_cast_floating_point testing",
+    "[template]",
+    lwm::internal::implicit_cast_floating_point,
+    ((double, int, int, int), (long, double, float, long), (double, double, double, float), (long, int, int, double)))
+{
+  REQUIRE(is_same<typename TestType::type, double>::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "include_signed testing",
+    "[template]",
+    lwm::internal::include_signed,
+    ((long, int), (unsigned int, int), (long, unsigned long, int), (unsigned int, unsigned int, unsigned, int)))
+{
+  REQUIRE(TestType::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "include_signed testing",
+    "[template]",
+    lwm::internal::include_signed,
+    ((unsigned, unsigned long),
+     (unsigned int, unsigned),
+     (unsigned, unsigned long, unsigned int),
+     (unsigned int, unsigned int, unsigned, unsigned)))
+{
+  REQUIRE(TestType::value == false);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "implicit_cast_fixed_point testing",
+    "[template]",
+    lwm::internal::implicit_cast_fixed_point,
+    ((unsigned int, int, char, int), (char, char, short, int), (int, int, int, int), (short, short, unsigned int, short)))
+{
+  REQUIRE(is_same<typename TestType::type, int>::value == true);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE(
+    "implicit_cast_fixed_point testing",
+    "[template]",
+    lwm::internal::implicit_cast_fixed_point,
+    ((unsigned int, unsigned short, unsigned int, unsigned short), (unsigned, unsigned, unsigned, unsigned)))
+{
+  REQUIRE(is_same<typename TestType::type, unsigned int>::value == true);
 }
