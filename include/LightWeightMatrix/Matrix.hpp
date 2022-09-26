eecc45608034bbafc36d5dde60ac0e87b36bc27d
timetraveler <timetraveler930@gmail.com>
2022-09-26 16:42:48 +0900

    Matrix Class constructor

--- include/LightWeightMatrix/Matrix.hpp
+++ include/LightWeightMatrix/Matrix.hpp
@@ -21,7 +21,7 @@ namespace lwm
 {
   using namespace internal;
   template<size_t N>
-  using const_size_t = integral_constant<size_t, N>;
+  using const_size_t = typename integral_constant<size_t, N>::type;
   /**
    * @brief A template based Matrix class
    *
@@ -33,26 +33,59 @@ namespace lwm
   class Matrix
   {
     T data[M][N]{};
-    template<size_t R, size_t C, size_t r, size_t c>
-    static void size_static_assert()
-    {
-      static_assert(((M >= (R + r)) && (N >= (C + c))), "Matrix Size Error.");
-    }
-    template<size_t R, size_t C>
-    static void size_assert(const size_t& r, const size_t& c)
-    {
-      assert((M >= (R + r)) && (N >= (C + c)));
-    }
+    typedef const T data_array[N];
+    template<typename, size_t, size_t>
+    friend class Matrix;
 
    public:
     Matrix() = default;
-    template<typename U>
-    explicit Matrix(const U (&in)[M * N])
+
+    /**
+     * @brief Construct a new Matrix object
+     *
+     * @tparam U type of 1D array.
+     * @tparam L length of 1D array, supports compile-time checking for size.
+     */
+    template<typename U, size_t L>
+    explicit Matrix(const U (&in)[L])
     {
-      for (size_t i = 0; i < M; i++)
-        for (size_t j = 0; j < N; j++)
-          data[i][j] = static_cast<allowed_cast_t<T, U>>(in[N * i + j]);
+      size_t m{ 0 }, n{ 0 };
+      for (size_t i = 0; i < L; i++)
+      {
+        data[m][n] = static_cast<allowed_cast_t<T, U>>(in[i]);
+        if (++n == N)
+        {
+          m++;
+          n = 0;
+        }
+      }
     }
+    /**
+     * @brief Construct a new Matrix object
+     *
+     * @tparam U type of 1D array.
+     * @tparam L length of 1D array, supports compile-time checking for size.
+     */
+    template<typename U, size_t L>
+    Matrix<T, M, N>& operator=(const U (&in)[L])
+    {
+      size_t m{ 0 }, n{ 0 };
+      for (size_t i = 0; i < L; i++)
+      {
+        data[m][n] = static_cast<allowed_cast_t<T, U>>(in[i]);
+        if (++n == N)
+        {
+          m++;
+          n = 0;
+        }
+      }
+      return (*this);
+    }
+    /**
+     * @brief Construct a new Matrix object
+     *
+     * @tparam U type of 2D array.
+     */
     template<typename U>
     explicit Matrix(const U (&in)[M][N])
     {
@@ -60,33 +93,73 @@ namespace lwm
         for (size_t j = 0; j < N; j++)
           data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i][j]);
     }
+    /**
+     * @brief Construct a new Matrix object, do not supports assert from size of array, recommend to use const_size_t type.
+     *
+     * @tparam U type of 2D array.
+     * @tparam R row of input array
+     * @tparam C col of input array
+     * @param r first row index of data.
+     * @param c first col index of data.
+     */
+    template<typename U, size_t R, size_t C, typename type_ = size_t, typename = enable_if_t<are_fixed_point<type_>::value>>
+    explicit Matrix(const U (&in)[R][C], type_ r_ = type_(0), type_ c_ = type_(0))
+    {
+      const size_t r = static_cast<size_t>(r_);
+      const size_t c = static_cast<size_t>(c_);
+      size_static_assert<R, C, 0, 0>();
+      size_assert<R, C>(r, c);
+      // Todo :: Call SetZero?
+      for (size_t i = r; i < r + R; i++)
+        for (size_t j = c; j < c + C; j++)
+          data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i - r][j - c]);
+    };
+    /**
+     * @brief Corresponding copy constructor of the constructor Matrix(const U (&in)[R][C])
+     *
+     * @tparam U type of 2D array.
+     * @tparam R row of input array.
+     * @tparam C col of input array
+     */
     template<typename U, size_t R, size_t C>
-    explicit Matrix(const U (&in)[R][C])
+    Matrix<T, M, N>& operator=(const U (&in)[R][C])
     {
       size_static_assert<R, C, 0, 0>();
       // Todo :: Call SetZero?
       for (size_t i = 0; i < R; i++)
         for (size_t j = 0; j < C; j++)
           data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i][j]);
-    };
-    template<typename U, size_t R, size_t C>
-    explicit Matrix(const U (&in)[R][C], size_t r, size_t c)
+      return (*this);
+    }
+    /**
+     * @brief Construct a new Matrix object, supports static assert from sizes of array and start index of row and col.
+     *
+     * @tparam U type of 2D array.
+     * @tparam R row of input array
+     * @tparam C col of input array
+     * @param r first row index of data.
+     * @param c first col index of data.
+     */
+    template<
+        typename U,
+        size_t R,
+        size_t C,
+        typename r,
+        typename c,
+        typename = enable_if_t<is_same<const_size_t<r::value>, r>::value && is_same<const_size_t<c::value>, c>::value, void>>
+    explicit Matrix(const U (&in)[R][C], r, c)
     {
-      size_assert<R, C>(r, c);
+      size_static_assert<R, C, r::value, c::value>();
       // Todo :: Call SetZero?
-      for (size_t i = r; i < r + R; i++)
-        for (size_t j = c; j < c + C; j++)
-          data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i][j]);
+      for (size_t i = r::value; i < r::value + R; i++)
+        for (size_t j = c::value; j < c::value + C; j++)
+          data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i - r::value][j - c::value]);
     };
-    template<typename U, size_t R, size_t C, size_t r, size_t c>
-    explicit Matrix(const T (&in)[R][C], const_size_t<r>, const_size_t<c>)
-    {
-      size_static_assert<R, C, r, c>();
-      // Todo :: Call SetZero?
-      for (size_t i = r; i < r + R; i++)
-        for (size_t j = c; j < c + C; j++)
-          data[i][j] = static_cast<allowed_cast_t<T, U>>(in[i][j]);
-    };
+    /**
+     * @brief Copy Constructor from other type of Matrix Object.
+     *
+     * @tparam U type of Matrix object.
+     */
     template<typename U>
     explicit Matrix(const Matrix<U, M, N>& in)
     {
@@ -94,6 +167,12 @@ namespace lwm
         for (size_t j = 0; j < N; j++)
           data[i][j] = static_cast<allowed_cast_t<T, U>>(in.data[i][j]);
     }
+    /**
+     * @brief Copy constructor(operator=) from same type of Matrix object.
+     *
+     * @param in Matrix object
+     * @return Matrix<T, M, N>& current Matrix object.
+     */
     Matrix<T, M, N>& operator=(const Matrix<T, M, N>& in)
     {
       if (this != const_cast<Matrix<T, M, N>*>(&in))
@@ -102,6 +181,12 @@ namespace lwm
             data[i][j] = in.data[i][j];
       return (*this);
     }
+    /**
+     * @brief Copy constructor(operator=) from other type of Matrix object.
+     *
+     * @tparam U type of Matrix object
+     * @return Matrix<T, M, N>& current Matrix object.
+     */
     template<typename U>
     Matrix<T, M, N>& operator=(const Matrix<U, M, N>& in)
     {
@@ -110,8 +195,43 @@ namespace lwm
           data[i][j] = static_cast<allowed_cast_t<T, U>>(in.data[i][j]);
       return (*this);
     }
-    template<typename, size_t, size_t>
-    friend class Matrix;
-  };
 
+    /**
+     * @brief Accessors by()
+     *
+     * @param i rows
+     * @param j cols
+     * @return const T& data[i][j]
+     */
+    inline const T& operator()(size_t i, size_t j) const
+    {
+      assert(i < M);
+      assert(j < N);
+
+      return data[i][j];
+    }
+    /**
+     * @brief Accessors by [][], it dose not support column size checking.
+     *
+     * @param i rows
+     * @return const T*&
+     */
+    inline data_array& operator[](size_t i) const
+    {
+      assert(i < M);
+      return data[i];
+    }
+
+   private:
+    template<size_t R, size_t C, size_t r, size_t c>
+    static void size_static_assert()
+    {
+      static_assert(((M >= (R + r)) && (N >= (C + c))), "Matrix Size Error.");
+    }
+    template<size_t R, size_t C, typename type_ = size_t, typename = enable_if_t<are_fixed_point<type_>::value>>
+    static void size_assert(const type_& r, const type_& c)
+    {
+      assert((M >= (R + static_cast<size_t>(r))) && (N >= (C + static_cast<size_t>(c))));
+    }
+  };
 };  // namespace lwm
--- test/src/Matrix.cpp
+++ test/src/Matrix.cpp
@@ -2,53 +2,125 @@
 #include <LightWeightMatrix/Matrix.hpp>
 #include <catch2/catch.hpp>
 
-TEST_CASE( "vectors can be sized and resized", "[lwm::Matrix]" ) {
+using namespace lwm;
+using namespace lwm::internal;
 
-    // Constructor build check
-    float arr1[6] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
-    lwm::Matrix<float, 3, 2> matrix1{arr1};    
-    float arr2[3][2] = {{1.0f, 2.0f}, {3.0f, 4.0f}, {5.0f, 6.0f}};
-    lwm::Matrix<float, 3, 2> matrix2{arr2};
-    double arr3[6] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-    lwm::Matrix<float, 3, 2> matrix3{arr3};    
-    double arr4[3][2] = {{1.0, 2.0}, {3.0, 4.0}, {5.0, 6.0}};
-    lwm::Matrix<float, 3, 2> matrix4{arr4};
-    lwm::Matrix<double, 3, 2> matrix5;
-    matrix4 = matrix3;
-    matrix5 = matrix4;
+TEST_CASE("Matrix", "[lwm::Matrix]")
+{
+  SECTION("Constructor 1) explicit Matrix(const U (&in)[L])")
+  {
+    // Compile-time errors, error by casting type
+    // float arr1_temp[5] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+    // lwm::Matrix<uint32_t, 2, 3> matrix_temp{arr1_temp};
+    // double arr2_temp[7] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0 };
+    // lwm::Matrix<float, 2, 3> matrix_temp{arr2_temp};
 
-    lwm::Matrix<int, 3, 2> matrix7;
+    float arr[6] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f };
+    lwm::Matrix<float, 2, 3> matrix{ arr };
+    matrix = arr;
+    REQUIRE(matrix(0, 0) == arr[0]);
+    REQUIRE(matrix(0, 1) == arr[1]);
+    REQUIRE(matrix(0, 2) == arr[2]);
+    REQUIRE(matrix(1, 0) == arr[3]);
+    REQUIRE(matrix(1, 1) == arr[4]);
+    REQUIRE(matrix(1, 2) == arr[5]);
+    REQUIRE(matrix[0][0] == arr[0]);
+    REQUIRE(matrix[0][1] == arr[1]);
+    REQUIRE(matrix[0][2] == arr[2]);
+    REQUIRE(matrix[1][0] == arr[3]);
+    REQUIRE(matrix[1][1] == arr[4]);
+    REQUIRE(matrix[1][2] == arr[5]);
 
-    // How to not possible this method?
-    printf("WTF? \n");
-    float arr10[2][1];
-    lwm::Matrix<float, 3, 2> matrix10{arr10, lwm::const_size_t<1>(), lwm::const_size_t<1>()};
-    // matrix7.testfunc();
+    double arr2[5] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+    lwm::Matrix<float, 2, 3> matrix2{ arr2 };
+    matrix2 = arr2;
+    REQUIRE(matrix2(0, 0) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[0]));
+    REQUIRE(matrix2(0, 1) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[1]));
+    REQUIRE(matrix2(0, 2) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[2]));
+    REQUIRE(matrix2(1, 0) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[3]));
+    REQUIRE(matrix2(1, 1) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[4]));
+    REQUIRE(matrix2[0][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[0]));
+    REQUIRE(matrix2[0][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[1]));
+    REQUIRE(matrix2[0][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[2]));
+    REQUIRE(matrix2[1][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[3]));
+    REQUIRE(matrix2[1][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[4]));
 
-    SECTION( "Constructor Check" ) {
-        
+    double arr3[1] = { 1.0 };
+    lwm::Matrix<float, 2, 3> matrix3{ arr3 };
+    matrix3 = arr3;
+    REQUIRE(matrix3(0, 0) == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[0]));
+    REQUIRE(matrix3[0][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[0]));
 
-        // v.resize( 10 );
+    int arr4[5] = { 1, 2, 3, 4, 5 };
+    lwm::Matrix<float, 2, 3> matrix4{ arr4 };
+    matrix4 = arr4;
+    REQUIRE(matrix4(0, 0) == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[0]));
+    REQUIRE(matrix4(0, 1) == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[1]));
+    REQUIRE(matrix4(0, 2) == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[2]));
+    REQUIRE(matrix4(1, 0) == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[3]));
+    REQUIRE(matrix4(1, 1) == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[4]));
+    REQUIRE(matrix4[0][0] == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[0]));
+    REQUIRE(matrix4[0][1] == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[1]));
+    REQUIRE(matrix4[0][2] == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[2]));
+    REQUIRE(matrix4[1][0] == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[3]));
+    REQUIRE(matrix4[1][1] == static_cast<lwm::internal::allowed_cast_t<float, int>>(arr4[4]));
+  }
+  SECTION(
+      "Constructor 2) explicit const U (&in)[M][N], Matrix(const U (&in)[R][C], type_ r_ = type_(0), type_ c_ = type_(0))")
+  {
+    float arr[2][3] = { { 1.0f, 2.0f, 3.0f }, { 4.0f, 5.0f, 6.0f } };
+    lwm::Matrix<float, 2, 3> matrix{ arr };
+    matrix = arr;
+    REQUIRE(matrix(0, 0) == arr[0][0]);
+    REQUIRE(matrix(0, 1) == arr[0][1]);
+    REQUIRE(matrix(0, 2) == arr[0][2]);
+    REQUIRE(matrix(1, 0) == arr[1][0]);
+    REQUIRE(matrix(1, 1) == arr[1][1]);
+    REQUIRE(matrix(1, 2) == arr[1][2]);
 
-        // REQUIRE( v.size() == 10 );
-        // REQUIRE( v.capacity() >= 10 );
-    }
-    SECTION( "resizing smaller changes size but not capacity" ) {
-        // v.resize( 0 );
+    double arr2[2][3] = { { 1.0, 2.0, 3.0 }, { 4.0, 5.0, 6.0 } };
+    matrix = arr2;
+    REQUIRE(matrix[0][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[0][0]));
+    REQUIRE(matrix[0][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[0][1]));
+    REQUIRE(matrix[0][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[0][2]));
+    REQUIRE(matrix[1][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[1][0]));
+    REQUIRE(matrix[1][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[1][1]));
+    REQUIRE(matrix[1][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr2[1][2]));
 
-        // REQUIRE( v.size() == 0 );
-        // REQUIRE( v.capacity() >= 5 );
-    }
-    SECTION( "reserving bigger changes capacity but not size" ) {
-        // v.reserve( 10 );
+    // check Compile time error
+    // double arr3[3][3] = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
 
-        // REQUIRE( v.size() == 5 );
-        // REQUIRE( v.capacity() >= 10 );
-    }
-    SECTION( "reserving smaller does not change size or capacity" ) {
-        // v.reserve( 0 );
+    double arr3[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+    lwm::Matrix<float, 2, 3> matrix2{ arr3, int(0), int(0) };
+    matrix2 = arr3;
+    REQUIRE(matrix2[0][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[0][0]));
+    REQUIRE(matrix2[0][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[0][1]));
+    REQUIRE(matrix2[1][0] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[1][0]));
+    REQUIRE(matrix2[1][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr3[1][1]));
 
-        // REQUIRE( v.size() == 5 );
-        // REQUIRE( v.capacity() >= 5 );
-    }
+    double arr4[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+    lwm::Matrix<float, 4, 5> matrix3{ arr4, size_t(1), size_t(1) };
+    REQUIRE(matrix3[1][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[0][0]));
+    REQUIRE(matrix3[1][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[0][1]));
+    REQUIRE(matrix3[2][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[1][0]));
+    REQUIRE(matrix3[2][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[1][1]));
+
+    // check Compile time error
+    // float arr5[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+    // lwm::Matrix<float, 4, 5> matrix4{ arr5, lwm::const_size_t<3>(),  lwm::const_size_t<2>()};
+
+    float arr5[2][2] = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+
+    lwm::Matrix<float, 4, 5> matrix4{ arr5, lwm::const_size_t<1>(), lwm::const_size_t<1>() };
+    REQUIRE(matrix3[1][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[0][0]));
+    REQUIRE(matrix3[1][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[0][1]));
+    REQUIRE(matrix3[2][1] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[1][0]));
+    REQUIRE(matrix3[2][2] == static_cast<lwm::internal::allowed_cast_t<float, double>>(arr4[1][1]));
+  }
+  SECTION("reserving bigger changes capacity but not size")
+  {
+  }
+  SECTION("reserving smaller does not change size or capacity")
+  {
+  }
 }
