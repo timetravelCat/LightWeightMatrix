692a0d2397bd073661b83d209c3844b27627d3cf
timetraveler <timetraveler930@gmail.com>
2022-10-21 20:35:29 +0900

    Add Quaternion Class

--- include/LightWeightMatrix/Quaternion.hpp
+++ include/LightWeightMatrix/Quaternion.hpp
@@ -61,11 +61,159 @@ namespace lwm
       q[3] = other[3];
     }
 
+    template<typename U>
+    void operator=(const Quaternion<U>& other)
+    {
+      Quaternion& q = *this;
+      q[0] = other[0];
+      q[1] = other[1];
+      q[2] = other[2];
+      q[3] = other[3];
+    }
+
     // Rotation Matrix Constructor
+    template<typename U>
+    explicit Quaternion(const RotationMatrix<U>& R)
+    {
+      Quaternion& q = *this;
+      T t = R.trace();
+      if (t > T(0))
+      {
+        t = sqrt(T(1) + t);
+        q[0] = T(0.5) * t;
+        t = T(0.5) / t;
+        q[1] = (R(2, 1) - R(1, 2)) * t;
+        q[2] = (R(0, 2) - R(2, 0)) * t;
+        q[3] = (R(1, 0) - R(0, 1)) * t;
+      }
+      else if (R(0, 0) > R(1, 1) && R(0, 0) > R(2, 2))
+      {
+        t = sqrt(T(1) + R(0, 0) - R(1, 1) - R(2, 2));
+        q[1] = T(0.5) * t;
+        t = T(0.5) / t;
+        q[0] = (R(2, 1) - R(1, 2)) * t;
+        q[2] = (R(1, 0) + R(0, 1)) * t;
+        q[3] = (R(0, 2) + R(2, 0)) * t;
+      }
+      else if (R(1, 1) > R(2, 2))
+      {
+        t = sqrt(T(1) - R(0, 0) + R(1, 1) - R(2, 2));
+        q[2] = T(0.5) * t;
+        t = T(0.5) / t;
+        q[0] = (R(0, 2) - R(2, 0)) * t;
+        q[1] = (R(1, 0) + R(0, 1)) * t;
+        q[3] = (R(2, 1) + R(1, 2)) * t;
+      }
+      else
+      {
+        t = sqrt(T(1) - R(0, 0) - R(1, 1) + R(2, 2));
+        q[3] = T(0.5) * t;
+        t = T(0.5) / t;
+        q[0] = (R(1, 0) - R(0, 1)) * t;
+        q[1] = (R(0, 2) + R(2, 0)) * t;
+        q[2] = (R(2, 1) + R(1, 2)) * t;
+      }
+    }
 
     // Euler Angle Constructor
-
     // Axis Angle Constructor
+
+    // Other Methods
+    template<typename U>
+    Quaternion<implicit_cast_t<T, U>> operator*(const Quaternion<U>& p) const
+    {
+      const Quaternion& q = *this;
+      return Quaternion<implicit_cast_t<T, U>>{ q(0) * p(0) - q(1) * p(1) - q(2) * p(2) - q(3) * p(3),
+                                                q(1) * p(0) + q(0) * p(1) - q(3) * p(2) + q(2) * p(3),
+                                                q(2) * p(0) + q(3) * p(1) + q(0) * p(2) - q(1) * p(3),
+                                                q(3) * p(0) - q(2) * p(1) + q(1) * p(2) + q(0) * p(3) };
+    }
+
+    template<typename U>
+    void operator*=(const Quaternion<U>& other)
+    {
+      Quaternion& self = *this;
+      self = self * other;
+    }
+
+    static Quaternion exp(const Vector3<T>& u)
+    {
+        const T tol = T(0.2);           // ensures an error < 10^-10
+
+        T u_norm = u.norm();
+        T sinc_u, cos_u;
+
+        if (u_norm < tol) {
+            T u2 = u_norm * u_norm;
+            T u4 = u2 * u2;
+            T u6 = u4 * u2;
+
+            const T c2 = T(1.0 / 2.0);      // 1 / 2!
+            const T c3 = T(1.0 / 6.0);      // 1 / 3!
+            const T c4 = T(1.0 / 24.0);     // 1 / 4!
+            const T c5 = T(1.0 / 120.0);    // 1 / 5!
+            const T c6 = T(1.0 / 720.0);    // 1 / 6!
+            const T c7 = T(1.0 / 5040.0);   // 1 / 7!
+
+            // compute the first 4 terms of the Taylor serie
+            sinc_u = T(1.0) - u2 * c3 + u4 * c5 - u6 * c7;
+            cos_u = T(1.0) - u2 * c2 + u4 * c4 - u6 * c6;
+        } else {
+            sinc_u = T(sin(u_norm) / u_norm);
+            cos_u = T(cos(u_norm));
+        }
+        const Vector<T, 3> v = sinc_u * u;
+        return Quaternion{cos_u, v(0), v(1), v(2)};
+    };
+
+    Vector3<T> log() const 
+    {
+      const Quaternion& q = *this;
+      const T ang = acos(q(0));
+      if(abs(ang) < epsilon<T>())
+        return Vector3<T>::Zero();
+      return imag()*ang/sin(ang);
+    }
+
+    Quaternion inv() const
+    {
+      const Quaternion& q = *this;
+      return Quaternion(q(0) , -q(1), -q(2), -q(3));
+    }
+
+    Quaternion normalized() const
+    {
+      const Quaternion& q = *this;
+      T normSq = q.dot(q);
+      return Quaternion(q(0) / normSq, q(1) / normSq, q(2) / normSq, q(3) / normSq);
+    }
+
+    T real() const 
+    {
+      const Quaternion& q = *this;
+      return q(0);
+    }
+
+    Vector3<T> imag() const
+    {
+      return this->template slice<3, 1, 1, 0>();
+    }
+
+    template<typename U>
+    Vector3<U> rotate(const Vector3<U> &vec) const 
+    {
+        const Quaternion& q = *this;
+        Quaternion v(T(0), vec(0), vec(1), vec(2));
+        return (q*v*q.inv()).imag();
+    }
+
+    template<typename U>
+    Vector3<U> rotateReverse(const Vector3<U> &vec) const 
+    {
+        const Quaternion& q = *this;
+        Quaternion v(T(0), vec(0), vec(1), vec(2));
+        return (q.inv()*v*q).imag();
+    }
 
    private:
   };
--- test/src/Quaternion.cpp
+++ test/src/Quaternion.cpp
@@ -15,19 +15,19 @@ TEST_CASE("Quaternion", "[lwm::Quaternion]")
     Quaternion<float> quat;
     REQUIRE(quat(0) == 1.0f);
 
-    Quaternion<float> quat2{{0.0f, 1.0f, 0.0f, 0.0f}};
+    Quaternion<float> quat2{ { 0.0f, 1.0f, 0.0f, 0.0f } };
     REQUIRE(quat2(1) == 1.0f);
 
-    Vector<float, 4> vec1{{0.0f, 0.0f, 1.0f, 0.0f}};
-    Quaternion<float> quat3{vec1};
+    Vector<float, 4> vec1{ { 0.0f, 0.0f, 1.0f, 0.0f } };
+    Quaternion<float> quat3{ vec1 };
     REQUIRE(quat3(2) == 1.0f);
 
-    Vector<double, 4> vec2{{0.0f, 0.0f, 0.0f, 1.0f}};
-    Quaternion<float> quat4{vec2};
+    Vector<double, 4> vec2{ { 0.0f, 0.0f, 0.0f, 1.0f } };
+    Quaternion<float> quat4{ vec2 };
     REQUIRE(quat4(3) == float(1.0));
 
-    Quaternion<float> quat5{quat3};
-    Quaternion<float> quat6{quat4};
+    Quaternion<float> quat5{ quat3 };
+    Quaternion<float> quat6{ quat4 };
     REQUIRE(quat5(2) == float(1.0));
     REQUIRE(quat6(3) == float(1.0));
 
@@ -35,15 +35,62 @@ TEST_CASE("Quaternion", "[lwm::Quaternion]")
     REQUIRE(quat6(2) == float(1.0));
     quat6 = vec1;
     REQUIRE(quat6(2) == float(1.0));
-    
-    Quaternion<float> quat7{0.0f, 0.0f, 0.0f, 1.0f};
+
+    Quaternion<float> quat7{ 0.0f, 0.0f, 0.0f, 1.0f };
     REQUIRE(quat7(3) == 1.0f);
 
-    // Vector<float, 4> vector{{0.0f, 1.0f, 0.0f, 0.0f}};
-    // Quaternion<float> quat2 = vector;
-    // REQUIRE(quat2(0) == 0.0f);
-    // REQUIRE(quat2(1) == 1.0f);
-    // REQUIRE(quat2(2) == 0.0f);
-    // REQUIRE(quat2(3) == 0.0f);
+    RotationMatrixf rotmat = RotationMatrixf::Identity();
+    Quaternion<float> quat8{ rotmat };
+    REQUIRE(quat8(0) == 1.0f);
+    REQUIRE(quat8(1) == 0.0f);
+    REQUIRE(quat8(2) == 0.0f);
+    REQUIRE(quat8(3) == 0.0f);
+
+    Quaternion<float> quat9 = quat8*Quaternion<float>{0.0f, 1.0f, 0.0f, 0.0f};
+    REQUIRE(quat9(0) == 0.0f);
+    REQUIRE(quat9(1) == 1.0f);
+    REQUIRE(quat9(2) == 0.0f);
+    REQUIRE(quat9(3) == 0.0f);
+
+    auto quat10 = Quaternion<float>::exp(Vector3<float>{{0.0, 0.0, M_PI/2.0}});
+    REQUIRE(isSame(quat10(0), 0.0f) == true);
+    REQUIRE(isSame(quat10(1), 0.0f) == true);
+    REQUIRE(isSame(quat10(2), 0.0f) == true);
+    REQUIRE(quat10(3) == 1.0f);
+
+    auto vec0 = quat10.log();
+    REQUIRE(isSame(vec0(0), 0.0f) == true);
+    REQUIRE(isSame(vec0(1), 0.0f) == true);
+    REQUIRE(isSame(vec0(2), float(M_PI/2.0)) == true);
+
+    auto quat11 = quat9.inv();
+    REQUIRE(quat11(0) == 0.0f);
+    REQUIRE(quat11(1) == -1.0f);
+    REQUIRE(quat11(2) == 0.0f);
+    REQUIRE(quat11(3) == 0.0f);
+
+    auto quat12 = quat9.normalized();
+    REQUIRE(quat12(0) == 0.0f);
+    REQUIRE(quat12(1) == 1.0f);
+    REQUIRE(quat12(2) == 0.0f);
+    REQUIRE(quat12(3) == 0.0f);
+
+    float real = quat9.real();
+    REQUIRE(real == 0.0f);
+
+    Vector3<float> imag = quat9.imag();
+    REQUIRE(imag(0) == 1.0f);
+    REQUIRE(imag(1) == 0.0f);
+    REQUIRE(imag(2) == 0.0f);
+
+    auto rotvec0 = quat9.rotate(imag);
+    REQUIRE(rotvec0(0) == 1.0f);
+    REQUIRE(rotvec0(1) == 0.0f);
+    REQUIRE(rotvec0(2) == 0.0f);
+
+    auto rotvec1 = quat9.rotateReverse(imag);
+    REQUIRE(rotvec1(0) == 1.0f);
+    REQUIRE(rotvec1(1) == 0.0f);
+    REQUIRE(rotvec1(2) == 0.0f);
   }
 }
